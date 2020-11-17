3a8a0b552eeeacec09d3ffd10db94da975159a90
omar jaroudi <o.jaroudi@hotmail.com>
2020-11-17 18:57:29 +0200

    Added icon

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ Created on Thu Nov 12 16:20:00 2020
 """
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 from GUI import GUI
 
 def run():
    app = QApplication(sys.argv)
    
-   gui = GUI("b7bf3a5df43989dd97f0fa97ebce2f4a", "603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4")
+   gui = GUI("", "")
    gui.show()
-   
+   app.setWindowIcon(QIcon('key_icon.ico'))
+
    sys.exit(app.exec_())
 	
 if __name__ == '__main__':
